484d27a877735c79cbb17803100f96654c9094be
northes <me@northes.io>
2024-01-11 16:09:04 +0800

    feat: light/dark mode switch

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,10 +5,13 @@
 	import Footer from './Footer.svelte';
 	import QA from './QA.svelte';
 	import { Toaster } from 'svelte-french-toast';
+	import { ModeWatcher } from "mode-watcher";
+
 
 </script>
 
 <Toaster/>
+<ModeWatcher />
 
 <div class="flex flex-row justify-center p-4">
 	<div class="w-full md:w-1/2 max-w-[500px] space-y-6 ">
--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,32 +1,39 @@
 <script lang="ts">
-	import {Button} from '$lib/components/ui/button'
+	import { Button } from '$lib/components/ui/button';
+	import { mode, toggleMode } from 'mode-watcher';
 </script>
 
 <div class="flex flex-col items-center gap-2 text-sm text-muted-foreground cursor-pointer font-serif">
-		<div class="flex space-x-1.5 justify-center">
-			<div>
-				<Button variant="link" class="p-0 m-0 text-muted-foreground h-0">Light</Button>
-			</div>
-			<div>
-				.
-			</div>
-			<div>
-				<Button variant="link" class="p-0 m-0 text-muted-foreground h-0">Language</Button>
-			</div>
-			<div>
-				.
-			</div>
-			<div>
-				<a target="_blank" href="https://github.com/northes/is_the_work_worth" class="hover:underline underline-offset-4">Github</a>
-			</div>
-			<div>
-				.
-			</div>
-			<div>
-				<a target="_blank" href="https://vercel.com/" class="hover:underline underline-offset-4">Vercel</a>
-			</div>
+	<div class="flex space-x-1.5 justify-center">
+		<div>
+			<Button variant="link" on:click={toggleMode} class="p-0 m-0 text-muted-foreground h-0">
+				{#if $mode === 'light'}
+					Light
+				{:else }
+					Dark
+				{/if}
+			</Button>
 		</div>
 		<div>
-			Power by 🍞 <a class="hover:underline underline-offset-4" href="https://northes.io" target="_blank">northes</a>.
+			.
 		</div>
+		<div>
+			<Button variant="link" class="p-0 m-0 text-muted-foreground h-0">Language</Button>
+		</div>
+		<div>
+			.
+		</div>
+		<div>
+			<a target="_blank" href="https://github.com/northes/is_the_work_worth" class="hover:underline underline-offset-4">Github</a>
+		</div>
+		<div>
+			.
+		</div>
+		<div>
+			<a target="_blank" href="https://vercel.com/" class="hover:underline underline-offset-4">Vercel</a>
+		</div>
+	</div>
+	<div>
+		Power by 🍞 <a class="hover:underline underline-offset-4" href="https://northes.io" target="_blank">northes</a>.
+	</div>
 </div>
